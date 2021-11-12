f7b5a22372dee09fc021d98a5ef5b0b94eb48b5c
Gugustinette <mercier.augustin@outlook.fr>
2021-11-12 10:19:49 +0100

    Avoid Bats Init - 2.1

---
+++ AvoidBats/game.py
@@ -0,0 +1,30 @@
+# Fichier principal
+
+
+# Import des modules
+import pygame
+from game_config import *
+
+
+# Boucle principale
+def game_loop():
+    quitting = False
+    while not quitting :
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT :
+                quitting = True
+
+# Configuration et lancement du jeu
+def main() :
+    pygame.init()
+
+    window = pygame.display.set_mode((GameConfig.WINDOW_W,GameConfig.WINDOW_H))
+    pygame.display.set_caption("Avoid Bats")
+
+    game_loop()
+
+    pygame.quit()
+    quit()
+
+
+main()
---
+++ AvoidBats/game_config.py
@@ -0,0 +1,6 @@
+# Class Game Config
+#       - Contiens les constantes de jeu
+
+class GameConfig :
+    WINDOW_H = 640
+    WINDOW_W = 960

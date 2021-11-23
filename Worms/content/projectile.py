910f14524805c74f2f78ec65ebda014080c33f31
E201139E <jasonmoulin37@gmail.com>
2021-11-23 18:53:19 +0100

    projectiles

--- Worms/content/projectile.py
+++ Worms/content/projectile.py
@@ -12,8 +12,11 @@ import time
 
 class Projectile(Entity):
 
+    g = 9.7639
+
     def __init__(self, x, y, height, width, mass):
-        super().__init__(x, y, height, width, mass, "projectile_1.jpg")
+        super().__init__(x, y, height, width, mass, "missile.png")
+        self.cur_x, self.cur_y = pygame.mouse.get_pos()
 
     def draw(self, screen):
         super().draw(screen)
@@ -22,23 +25,43 @@ class Projectile(Entity):
         self.pull()
         super().advance_state()
 
-    def trajectoire(self, a, t, mass):
-        gravity = 9.7639
-        speed = mass * gravity
-        x = math.cos(a)*speed*t
-        y = -0.5*gravity*t**2+math.sin(a)*speed*t+self.rect.height
+    def trajectoire(self, t, mass):
+        # gravity = 9.7639
+        # speed = mass * gravity
+        # x = math.cos(a)*speed*t
+        # y = -0.5*gravity*t**2+math.sin(a)*speed*t+self.rect.height 
+ 
+        # return x,y
+        pi = 3.141592654
+ 
+        if self.rect.x == 0:
+            a = 90*pi/180
+ 
+        else:
+            a = 45*pi/180
+ 
+        y = self.rect.y/mass
+        x = self.rect.x/mass
+        v0 = math.sqrt(pow(x,2) + pow(y,2))
+        vy = self.g * t + v0* math.sin (a)
+        y = +1*(vy- 0.5*self.g*pow(t,2))
+        x = v0*math.cos(a) * t
+ 
+        self.rect.x = x + 100
+        self.rect.y = y
 
-        return x,y
+        print(self.rect.x , " || " , x)
+        print(self.rect.y, " || ", y)
+
 
     def pull(self):
-        cur_x, cur_y = pygame.mouse.get_pos()
-        hypotenuse = math.sqrt((self.rect.x - cur_x)**2 + (self.rect.y - cur_y)**2)
-        oppo = cur_y - 0
+        hypotenuse = math.sqrt((self.rect.x - self.cur_x)**2 + (self.rect.y - self.cur_y)**2)
+        oppo = self.cur_y - GameConfig.WINDOW_H
         angle = math.sin(oppo / hypotenuse)
         v0 = round(time.time() * 1000)
-        proj_x, proj_y = self.rect.x + self.rect.width, self.rect.y - (self.rect.height/2)
+        proj_x, proj_y = self.rect.x, self.rect.y
 
-        while (proj_y > 0):
-            proj_x, proj_y = self.trajectoire(angle, round(time.time() * 1000) - v0, self.mass)
-            self.rect.x = proj_x
-            self.rect.y = proj_y
+        while (self.rect.x < GameConfig.WINDOW_W and self.rect.y < GameConfig.WINDOW_H):
+            self.trajectoire(round(time.time() * 1000) - v0, self.mass)
+        #     self.rect.x = proj_x
+        #     self.rect.y = proj_y
